148eaaf71c4d9171052749bcf0ab08ad2dc976c1
cauidavismatosdeoliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2022-06-30 18:28:19 -0300

    commit

--- PDP/TM.java
+++ PDP/TM.java
@@ -1,8 +1,32 @@
 package PDP;
 
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class TM{
     public static void main(String[] args) {
-        Usuario u1 = new Usuario("Francoes", 25, "caxias", "CC7");
+        Usuario u1 = new Usuario();
+        u1.setNome("francois");
+        u1.setIdade(25);
+        u1.setCampus("caxias");
+        u1.setCurso("CC7");
         System.out.println(u1);
+        File arquivo = new File ("arquivo.txt");
+        try {
+            arquivo.createNewFile();
+            FileWriter fileWriter = new FileWriter(arquivo,false);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println("Nome: " + u1.getNome());
+            printWriter.println("Idade: " + u1.getIdade());
+            printWriter.println("Campus: " + u1.getCampus());
+            printWriter.println("Curso: " + u1.getCurso());
+            printWriter.flush();
+            printWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- PDP/Usuario.java
+++ PDP/Usuario.java
@@ -6,14 +6,41 @@ public class Usuario {
     private String Campus;
     private String Curso;
 
+    public Usuario(){
+    }
     public Usuario (String Nome, Integer Idade, String Campus, String Curso) {
         this.Nome = Nome;
         this.Idade = Idade;
         this.Campus = Campus;
         this.Curso = Curso;
     }
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+    public void setIdade(Integer Idade) {
+        this.Idade = Idade;
+    }
+    public void setCampus(String Campus) {
+        this.Campus = Campus;
+    }
+    public void setCurso(String Curso) {
+        this.Curso = Curso;
+    }
+    public String getNome() {
+        return Nome;
+    }
+    public Integer getIdade() {
+        return Idade;
+    }
+    public String getCampus() {
+        return Campus;
+    }
+    public String getCurso() {
+        return Curso;
+    }
     @Override
     public String toString(){
         return "\nNome: "+ Nome + "\nidade: "+ Idade + "\nCampus: "+ Campus + "\nCurso: "+ Curso;
     }
+    
 }
