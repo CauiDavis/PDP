894aeffc704d7d248703fa66c86f8da0a30c996e
cauidavismatosdeoliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2022-06-30 22:31:15 -0300

    commit

--- PDP/TM.java
+++ PDP/TM.java
@@ -15,8 +15,10 @@ public class TM{
         u1.setCurso("CC7");
         System.out.println(u1);
         File arquivo = new File ("arquivo.txt");
+        File arquivocsv = new File ("arquivocsv.csv");
         try {
             arquivo.createNewFile();
+            arquivocsv.createNewFile();
             FileWriter fileWriter = new FileWriter(arquivo,false);
             PrintWriter printWriter = new PrintWriter(fileWriter);
             printWriter.println("Nome: " + u1.getNome());
@@ -25,6 +27,14 @@ public class TM{
             printWriter.println("Curso: " + u1.getCurso());
             printWriter.flush();
             printWriter.close();
+            FileWriter fileWriter1 = new FileWriter(arquivocsv,false);
+            PrintWriter printWriter1 = new PrintWriter(fileWriter1);
+            printWriter1.println("Nome: " + u1.getNome());
+            printWriter1.println("Idade: " + u1.getIdade());
+            printWriter1.println("Campus: " + u1.getCampus());
+            printWriter1.println("Curso: " + u1.getCurso());
+            printWriter1.flush();
+            printWriter1.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
